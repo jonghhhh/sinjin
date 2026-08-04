dc6c0d83caec44292b136baf331ccb464278fe00
jonghhhh <31147933+jonghhhh@users.noreply.github.com>
2026-08-04 17:57:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script를 웹 앱으로 배포한 뒤 /exec로 끝나는 주소를 따옴표 안에 넣으세요.
-window.APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_URL_HERE";
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzTE6IZT7K7XxfuwdKlCxxLd67kMlqIZSPPRQ7XO1a5eMT4CTa2SmGyEdpJf2VBBYLoSA/exec";
